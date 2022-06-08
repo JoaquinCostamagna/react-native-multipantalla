c87c11666e031f3b0986681ffc0505ecf8ca4300
CamilaBermejo <68954151+CamilaBermejo@users.noreply.github.com>
2022-06-07 22:49:57 -0300

    AlbumList  clase -> función

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import "react-native-gesture-handler";
 import React from "react";
 
+<<<<<<< Updated upstream
 import AlbumList from "./components/AlbumList";
 import PhotoList from "./components/PhotoListFunc";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+=======
+import AlbumList from './components/AlbumListFunc';
+import PhotoList from './components/PhotoList';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+>>>>>>> Stashed changes
 
 const Stack = createStackNavigator();
 
---
+++ components/AlbumListFunc.js
@@ -0,0 +1,39 @@
+import React, {Component, useEffect, useState} from 'react';
+import {ScrollView, Text, View} from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+const AlbumList = (props) => {
+    const [photoset, setPhotoset] = useState(null);
+    useEffect(() => {
+        axios
+            .get(
+                'https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=6e8a597cb502b7b95dbd46a46e25db8d&user_id=137290658%40N08&format=json&nojsoncallback=1',
+            )
+            .then((response) =>
+                setPhotoset(response.data.photosets.photoset),
+            );
+    });
+
+    const renderAlbums = () => {
+        return photoset.map((album) => (
+        <AlbumDetail
+            navigation={props.navigation}
+            key={album.id}
+            title={album.title._content}
+            albumId={album.id}
+        />
+        ));
+    }
+
+    if (!photoset) {
+        return <Text>Loading...</Text>;
+    }
+    return (
+        <View style={{flex: 1}}>
+        <ScrollView>{renderAlbums()}</ScrollView>
+        </View>
+    );
+}
+
+export default AlbumList;
