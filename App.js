fc8de14fc95b01d57b6354c806389a023e23e216
JoaquinCostamagna <jncostamagna@gmail.com>
2022-06-07 22:14:13 -0300

    PhotoList clase -> función

--- App.js
+++ App.js
@@ -1,30 +1,30 @@
-import 'react-native-gesture-handler';
-import React from 'react';
+import "react-native-gesture-handler";
+import React from "react";
 
-import AlbumList from './components/AlbumList';
-import PhotoList from './components/PhotoList';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import AlbumList from "./components/AlbumList";
+import PhotoList from "./components/PhotoListFunc";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
 // Create a component
 function App() {
   return (
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen
-        name="albumList"
-        component={AlbumList}
-        options={{title: 'Albums'}}
-      />
-      <Stack.Screen
-        name="photoList"
-        component={PhotoList}
-        options={{title: 'Photos'}}
-      />
-    </Stack.Navigator>
-  </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="albumList"
+          component={AlbumList}
+          options={{ title: "Albums" }}
+        />
+        <Stack.Screen
+          name="photoList"
+          component={PhotoList}
+          options={{ title: "Photos" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
